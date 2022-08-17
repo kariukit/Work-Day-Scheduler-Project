c74ea92f959720243076c1c78f5b3a0e27a82c41
kariukit <nyawirakariukit@gmail.com>
2022-08-16 23:06:54 -0400

    adding files

---
+++ script.js
@@ -0,0 +1,34 @@
+$(function () {
+   
+ function updateHour(){
+ $('#currentDay').text(moment().format('dddd, MMMM Do'));
+
+    let currentHour = moment().hours();
+     $('.time-block').each(function () {
+    var hourRow = parseInt($(this).attr('id').split('-')[1]);
+            if (hourRow< currentHour) {
+              $(this).addClass('past');
+            } 
+            if (hourRow === currentHour) {
+              $(this).removeClass('past')
+              $(this).addClass('present');
+            } 
+            if (hourRow> currentHour){
+              $(this).removeClass('present')
+              $(this).removeClass('past')
+              $(this).addClass('future');
+            }
+          });
+    }
+ updateHour();
+
+ $(document).on('click','.saveBtn',function(){
+     var SaveBtnValue = $(this).siblings('.description').val();
+     var desctiption = document.getElementsByClassName(SaveBtnValue).value;
+    localStorage.setItem(SaveBtnValue, desctiption);
+      
+    });
+
+    $('.event-saved').addClass('display');
+
+});
